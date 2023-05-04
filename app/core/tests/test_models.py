5770882705eb2a1a99eb6d1f77aef66a5b54ae73
Ubeydu <u.keles@gmail.com>
2023-05-04 11:29:09 +0300

    Added our custom user model. And, applied migrations to create relevant models.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,23 @@
+"""
+Tests for models.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test that checks that we can create a user with an email successfully."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password)) # We are not writing \
+            # 'user.password' like we did for email, because we need to \
+            # check the password through the hashing system.
