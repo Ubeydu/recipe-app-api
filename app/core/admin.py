0399ad6476fc7e15ce20fdf60338d9b36036e0ac
Ubeydu <u.keles@gmail.com>
2023-05-08 19:21:12 +0300

    Created base user class for our Django Admin.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin # noqa
+"""
+Django Admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+# This is to import all of the custom
+# models that we are going to register with our Django Admin.
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
